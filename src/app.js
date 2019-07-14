197f4811de5ca17194ad3bff572dc9667d1d8805
unknown <surajkum1198@gmail.com>
2019-07-14 19:47:33 +0530

    some changes

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const geocode=require('./utils/geocode')
 
 
 const app = express()
-const port = progress.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 //Define path for express config
 
